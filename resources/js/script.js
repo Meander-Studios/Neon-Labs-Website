7bd4e6c01d5b287387210e63e05ae37b3f07d399
Geekcoder127 <awesome2723@oultook.com>
2017-05-15 21:54:41 +0100

    Finished About Page

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,4 +5,5 @@ function goToPage(thePage){
   $('html, body').animate({
       scrollTop: $("#" + thePage.innerHTML + '-section').offset().top - $(window).height() * 0.15
   }, 500);
+  document.getElementById('theTitle').innerHTML = "Neon Labs | " + thePage.innerHTML;
 }
